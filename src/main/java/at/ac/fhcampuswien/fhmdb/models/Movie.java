b6dd8a8f8b3ff4377414a1b6e1727083bddd25db
Stephan Bollauf <stephan.bollauf@gmail.com>
2024-02-16 18:22:53 +0100

    add genres to movie class

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Movie {
+    private enum Genre{
+        ACTION, ADVENTURE, ANIMATION, BIOGRAPHY, COMEDY, CRIME, DRAMA, DOCUMENTARY, FAMILY, FANTASY, HISTORY, HORROR, MUSICAL, MYSTERY, ROMANCE, SCIENCE_FICTION, SPORT, THRILLER, WAR,WESTERN
+    }
     private String title;
     private String description;
-    // TODO add more properties here
+    private List<Genre> genres;
 
-    public Movie(String title, String description) {
+    public Movie(String title, String description, List<Genre> genres) {
         this.title = title;
         this.description = description;
+        this.genres = genres;
     }
 
     public String getTitle() {
@@ -20,6 +24,7 @@ public class Movie {
     public String getDescription() {
         return description;
     }
+    public List<Genre> getGenres() { return genres; }
 
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
