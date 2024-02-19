e242069a56d0fae9237a0c7dd1b5899c913485f3
Stephan Bollauf <stephan.bollauf@gmail.com>
2024-02-19 20:42:47 +0100

    redo project structure

--- src/test/java/MovieTest.java
+++ src/test/java/MovieTest.java
@@ -1,10 +1,7 @@
-package at.ac.fhcampuswien.fhmdb;
-
 import at.ac.fhcampuswien.fhmdb.models.Movie;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
