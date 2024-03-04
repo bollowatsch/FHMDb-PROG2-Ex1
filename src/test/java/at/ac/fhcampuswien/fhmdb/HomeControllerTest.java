5272225461b8fe9f130d835276697869766376fe
Nico <nico.orth@stud.fh-campuswien.ac.at>
2024-03-04 17:54:29 +0100

    added tests (filtering by genre)

--- src/test/java/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -1,15 +1,16 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import at.ac.fhcampuswien.fhmdb.models.Genre;
 import at.ac.fhcampuswien.fhmdb.models.Movie;
-import com.jfoenix.controls.JFXButton;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HomeControllerTest {
 
@@ -68,6 +69,7 @@ public class HomeControllerTest {
         ObservableList<Movie> expected = FXCollections.observableArrayList(new Movie("B"));
         assertEquals(actual, expected);
     }
+
     @Test
     public void test_sort_single_value_MovieList_descending_returns_single_value_list() {
         // given
@@ -80,5 +82,41 @@ public class HomeControllerTest {
         //then
         ObservableList<Movie> expected = FXCollections.observableArrayList(new Movie("A"));
         assertEquals(actual, expected);
+    }
+
+    @Test
+    public void test_filter_by_genre() {
+        //Arrange
+        HomeController hc = new HomeController();
+        ObservableList<Movie> movies = FXCollections.observableArrayList();
+        movies.add(new Movie("The Shawshank Redemption", "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.", new ArrayList<>(List.of(Genre.DRAMA))));
+        movies.add(new Movie("The Godfather", "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", new ArrayList<>(Arrays.asList(Genre.DRAMA, Genre.CRIME))));
+        movies.add(new Movie("12th fail", "The real-life story of IPS Officer Manoj Kumar Sharma and IRS Officer Shraddha Joshi.", new ArrayList<>(Arrays.asList(Genre.DRAMA, Genre.BIOGRAPHY))));
+
+        //Act
+        ObservableList<Movie> filteredList = hc.filterByGenre(movies, Genre.BIOGRAPHY);
+
+        //Assert
+        assertEquals(movies.get(2), filteredList.get(0));
+    }
+
+    @Test
+    public void test_filter_by_multiple_genres() {
+        //Arrange
+        ObservableList<Movie> movies = FXCollections.observableList(Movie.initializeMovies());
+        HomeController hc = new HomeController();
+        ArrayList<Movie> actual = new ArrayList<>();
+
+        //Act
+        actual.add(movies.get(5));
+        actual.add(movies.get(9));
+        actual.add(movies.get(10));
+        actual.add(movies.get(13));
+        actual.add(movies.get(14));
+
+        ObservableList<Movie> filteredList = hc.filterByGenre(movies, Genre.ADVENTURE); //should be 5 movies
+
+        //Assert
+        assertEquals(actual, filteredList);
     }
 }
