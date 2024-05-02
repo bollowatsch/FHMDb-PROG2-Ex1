1d667bb075bf19977480ef497a69d5a9b7183bca
Franziska-09 <franziska.gschweidl@stud.fh-campuswien.ac.at>
2024-05-02 19:42:42 +0200

    bugfix of export of database package in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,7 +9,8 @@ module at.ac.fhcampuswien.fhmdb {
     requires java.sql;
 
     opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
-    opens at.ac.fhcampuswien.fhmdb.models to com.google.gson, ormlite.jdbc;
+    opens at.ac.fhcampuswien.fhmdb.models to com.google.gson;
+    opens at.ac.fhcampuswien.fhmdb.database to ormlite.jdbc;
     exports at.ac.fhcampuswien.fhmdb.models;
     exports at.ac.fhcampuswien.fhmdb;
 }
